080371e1539d81b8cb17cdeee5ce6ee365e29f1c
Iago Augusto <58311839+IagoAugust@users.noreply.github.com>
2024-04-03 01:14:27 -0300

    Create listagem.php

---
+++ listagem.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "user_%";
+$password = "vagrant";
+$dbname = "formulario";
+$tablename = "cadastro";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname, 3308);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Prepara a consulta SQL para listar os registros
+$sql = "SELECT nome, email FROM $tablename";
+
+// Executa a consulta
+$result = $conn->query($sql);
+
+// Exibe os resultados
+if ($result->num_rows > 0) {
+    echo "<h1>Lista de Cadastros</h1>";
+    while ($row = $result->fetch_assoc()) {
+        echo "Nome: " . $row["nome"] . " - Email: " . $row["email"] . "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado";
+}
+
+// Fecha a conexão
+$conn->close();
+?>
